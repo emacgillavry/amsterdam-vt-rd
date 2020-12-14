8d3c3d5ebdbc3d1f62dda157c27f2ceb50d67465
Edward Mac Gillavry <edward@webmapper.net>
2020-12-14 14:26:52 +0100

    Drill down to zoom level 16 without overzooming the tiles.

--- app/src/index.js
+++ app/src/index.js
@@ -81,7 +81,7 @@ function rd() {
 function dutchTileGrid() {
   return new TileGrid({
     extent: [-285401.920, 22598.080,  595401.9199999999, 903401.9199999999],
-    resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105],
+    resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105,0.0525],
   });
 }
 
@@ -113,7 +113,7 @@ function pdokGrayMap(projection, tileGrid) {
 }
 
 function owObjectLayer(projection, tileGrid) {
-  const maxZoom = 10;
+  const maxZoom = 16;
   const source = new VectorTileSource({
     attributions: "<a href='https://www.kadaster.nl' target='_blank'>Kadaster</a>",
     format: new MVT(),
@@ -124,7 +124,7 @@ function owObjectLayer(projection, tileGrid) {
       resolutions: tileGrid.getResolutions().slice(0, maxZoom + 1),
     }),
     url: "http://localhost:6767/basiskaart_rd/{z}/{x}/{y}.pbf",
-    tilePixelRatio: 16,
+    tilePixelRatio: 1,
     renderBuffer: 10
   });
   return new VectorTileLayer({
