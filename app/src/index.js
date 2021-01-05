e8d7899cb1803520db7ec4d1b355856f1473914f
Edward Mac Gillavry <edward@webmapper.net>
2021-01-05 22:08:11 +0100

    Align viewer with Mapbox default tile size of 512 to compare easily with Mapbox GL JS viewer.

--- app/src/index.js
+++ app/src/index.js
@@ -83,7 +83,11 @@ function rd() {
 function dutchTileGrid() {
   return new TileGrid({
     extent: [-285401.920, 22598.080, 595401.9199999999, 903401.9199999999],
-    resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
+//  resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
+//                0         1         2        3        4        5        6       7       8       9      10     11     12     13     14     15     16
+    resolutions: [1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525, 0.02625],
+//                0         1        2        3        4        5       6       7       8      9      10     11     12     13     14     15      16
+    tileSize: 512,
   });
 }
 
@@ -124,6 +128,7 @@ function owObjectLayer(projection, tileGrid) {
       extent: tileGrid.getExtent(),
       origin: getTopLeft(tileGrid.getExtent()),
       resolutions: tileGrid.getResolutions().slice(0, maxZoom + 1),
+      tileSize: 512,
     }),
     url: "http://localhost:6768/basiskaart_rd/{z}/{x}/{y}.pbf",
     tilePixelRatio: 1,
